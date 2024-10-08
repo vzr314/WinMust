828ebe4cc122e924176f8772ffde4a87b120ea33
CyberianIce <cyberianice@gmail.com>
2024-10-08 09:31:09 +0200

    Elevated to .NET 4.8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinMust")]
+[assembly: AssemblyTitle("WinMust++")]
 [assembly: AssemblyDescription("Simple UPS Status Monitor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.2.2")]
-[assembly: AssemblyFileVersion("0.5.2.2")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
